ba22d70982e3c7577ca2be10b0c93675f4c42d9f
Badr <s156956@live.killeenisd.org>
2020-10-24 04:05:07 -0500

    removed application.properties

---
+++ src/main/java/comp/hacktx/backend/Utils.java
@@ -0,0 +1,15 @@
+package comp.hacktx.backend;
+
+public class Validate {
+
+    public static boolean validateUsername(String username) {
+        // TODO: Validate
+        return true;
+    }
+
+    public static boolean validatePassword(String password) {
+        // TODO: Validate
+        return true;
+    }
+
+}
