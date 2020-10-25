3d2f016d24669d89996d8a488e1a36c78fdea364
Badr <s156956@live.killeenisd.org>
2020-10-25 03:10:04 -0500

    Commented MainController

--- src/main/java/comp/hacktx/backend/controllers/MainController.java
+++ src/main/java/comp/hacktx/backend/controllers/MainController.java
@@ -33,31 +33,30 @@ public class MainController {
         this.symptomsRepository = symptomsRepository;
     }
 
-    @PostMapping("/test")
-    public ResponseEntity<?> postTest(@RequestBody Map<String, Object> data) {
-        return new ResponseEntity<>(data, HttpStatus.OK);
-    }
-
-    @GetMapping("/test/{data}")
-    public ResponseEntity<?> getTest(@PathVariable String data) {
-        return new ResponseEntity<>(data, HttpStatus.OK);
-    }
-
+    /**
+     * @param credentials Object encompassing username and password
+     * @return ResponseEntity with auth token on success, error message on failure.
+     */
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody Credentials credentials) {
+        // Ensure credentials are complete
+        if (!credentials.assertNonNull()) {
+            return error("invalid credentials", HttpStatus.BAD_REQUEST);
+        }
+
         // Validate username
         if (!Utils.validateUsername(credentials.getUsername())) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "invalid username"), HttpStatus.BAD_REQUEST);
+            return error("invalid username", HttpStatus.BAD_REQUEST);
         }
 
         // Validate password
         if (!Utils.validatePassword(credentials.getPassword())) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "invalid password"), HttpStatus.BAD_REQUEST);
+            return error("invalid password", HttpStatus.BAD_REQUEST);
         }
 
         // Check if user exists
         if (userRepository.existsByUsername(credentials.getUsername())) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "User already exists"), HttpStatus.BAD_REQUEST);
+            return error("user already exists", HttpStatus.BAD_REQUEST);
         }
 
         // Create and add user
@@ -67,58 +66,57 @@ public class MainController {
         User user = new User(id, username, hashedPassword, 0, 0, 0);
         userRepository.save(user);
 
-        // Generate auth token
-        String token = Utils.buildToken(user, key);
-
-        return new ResponseEntity<>(Collections.singletonMap("token", token), HttpStatus.OK);
+        return simpleResponse("token", Utils.buildToken(user, key), HttpStatus.OK);
     }
 
+    /**
+     * @param credentials Object encompassing username and password
+     * @return ResponseEntity with auth token on success, error message on failure.
+     */
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody Credentials credentials) {
+        // Ensure credentials are complete
         if (!credentials.assertNonNull()) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "invalid credentials"), HttpStatus.BAD_REQUEST);
+            return error("invalid credentials", HttpStatus.BAD_REQUEST);
         }
 
+        // Verify user exists
         if (!userRepository.existsByUsername(credentials.getUsername())) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "invalid credentials"), HttpStatus.BAD_REQUEST);
+            return error("invalid credentials", HttpStatus.BAD_REQUEST);
         }
 
         User user = userRepository.findByUsername(credentials.getUsername());
 
+        // Verify password
         if (!BCrypt.checkpw(credentials.getPassword(), user.getPassword())) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "invalid credentials"), HttpStatus.BAD_REQUEST);
+            return error("invalid credentials", HttpStatus.BAD_REQUEST);
         }
 
-        // Generate auth token
-        String token = Utils.buildToken(user, key);
-
-        return new ResponseEntity<>(Collections.singletonMap("token", token), HttpStatus.OK);
+        // Generate and return auth token upon success
+        return simpleResponse("token", Utils.buildToken(user, key), HttpStatus.OK);
     }
 
+    /**
+     * @param headers All headers present in request
+     * @param data Entire body of request
+     * @return ResponseEntity indicating failure or success of this request
+     */
     @PostMapping("/reset-password")
     public ResponseEntity<?> resetPassword(@RequestHeader Map<String, String> headers, @RequestBody Map<String, String> data) {
-        if (!headers.containsKey("authorization")) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "This call requires auth"), HttpStatus.UNAUTHORIZED);
+        ResponseEntity<?> verificationResult = verifyToken(headers, data);
+        if (verificationResult != null) {
+            return verificationResult;
         }
 
-        if (!data.containsKey("username") || !data.containsKey("new-password")) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "malformed data"), HttpStatus.BAD_REQUEST);
+        if (!data.containsKey("new-password")) {
+            return error("malformed data", HttpStatus.BAD_REQUEST);
         }
-
-        String authHeader = headers.get("authorization");
-
-        // Header in form {"authorization": "Bearer a.b.c"}
-        String token = authHeader.substring(7);
 
         String username = data.get("username");
         String newPassword = data.get("new-password");
 
-        if (!Utils.verifyToken(username, token, key)) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "Bad token"), HttpStatus.BAD_REQUEST);
-        }
-
         if (!Utils.validatePassword(newPassword)) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "invalid password"), HttpStatus.BAD_REQUEST);
+            return error("invalid password", HttpStatus.BAD_REQUEST);
         }
 
         // Update password
@@ -127,34 +125,31 @@ public class MainController {
         user.updatePassword(hashedPassword);
         userRepository.save(user);
 
-        return new ResponseEntity<>(Collections.singletonMap("success", "password updated"), HttpStatus.OK);
+        return simpleResponse("success", "password updated", HttpStatus.OK);
     }
 
+    /**
+     * @param headers All headers present in request
+     * @param data Entire body of request
+     * @return ResponseEntity indicating failure or success of this request
+     */
     @PostMapping("/report")
     public ResponseEntity<?> report(@RequestHeader Map<String, String> headers, @RequestBody Map<String, String> data) {
-        if (!headers.containsKey("authorization")) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "This call requires auth"), HttpStatus.UNAUTHORIZED);
+        ResponseEntity<?> verificationResult = verifyToken(headers, data);
+        if (verificationResult != null) {
+            return verificationResult;
         }
 
-        if (!data.containsKey("username") || !data.containsKey("zipcode") || !data.containsKey("symptoms")) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "malformed data"), HttpStatus.BAD_REQUEST);
+        if (!data.containsKey("zipcode") || !data.containsKey("symptoms")) {
+            return error("malformed data", HttpStatus.BAD_REQUEST);
         }
-
-        String authHeader = headers.get("authorization");
-
-        // Header in form {"authorization": "Bearer a.b.c"}
-        String token = authHeader.substring(7);
 
         String username = data.get("username");
         String zipcode = data.get("zipcode");
         String symptoms = data.get("symptoms");
 
-        if (!Utils.verifyToken(username, token, key)) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "Bad token"), HttpStatus.BAD_REQUEST);
-        }
-
         if (!Utils.isInteger(zipcode) || !Utils.isInteger(symptoms)) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "malformed data"), HttpStatus.BAD_REQUEST);
+            return error("malformed data", HttpStatus.BAD_REQUEST);
         }
 
         User user = userRepository.findByUsername(username);
@@ -162,12 +157,97 @@ public class MainController {
         // Get time since user made last report
         long deltaTime = System.currentTimeMillis() - user.getLastReport();
 
+        // If user already made a report within the last 24 hours, deny and return an error.
         if (deltaTime < 24 * 60 * 60 * 1000) {
             String errorMessage = "Must wait 24 hours between reports. Last report: " + user.getLastReport();
-            return new ResponseEntity<>(Collections.singletonMap("error", errorMessage), HttpStatus.BAD_REQUEST);
+            return error(errorMessage, HttpStatus.BAD_REQUEST);
         }
 
-        Report report = new Report(Long.toString(System.nanoTime()), System.currentTimeMillis(), Integer.parseInt(zipcode), Integer.parseInt(symptoms));
+        // Build and submit report
+        Report report = new Report(
+                Long.toString(System.nanoTime()),
+                System.currentTimeMillis(),
+                Integer.parseInt(zipcode),
+                Integer.parseInt(symptoms));
+
+        submitReport(user, report);
+
+        return simpleResponse("success", "report counted", HttpStatus.OK);
+    }
+
+    /**
+     * @param zipcode Zipcode to query for records
+     * @param start The beginning of the time window to query data from. Measured in milliseconds from epoch.
+     * @param end The ending of the time window to query data from. Measured in milliseconds from epoch.
+     * @return A ResponseEntity containing all relevant records, or containing an error if one exists.
+     */
+    @GetMapping("/records/{zipcode}/{start}/{end}")
+    public ResponseEntity<?> getReports(@PathVariable String zipcode, @PathVariable String start, @PathVariable String end) {
+        // Validate arguments
+        if (!Utils.isInteger(zipcode) || !Utils.isInteger(start) || !Utils.isInteger(end)) {
+            return error("malformed arg", HttpStatus.BAD_REQUEST);
+        }
+
+        List<Report> reports =
+                symptomsRepository.findAllByTimeBeforeAndTimeAfterAndZipcode(Long.parseLong(end), Long.parseLong(start), Integer.parseInt(zipcode));
+
+        return simpleResponse("reports", reports, HttpStatus.OK);
+    }
+
+    /**
+     * @param headers All headers present in request
+     * @param body Full request body
+     * @return ResponseEntity object if the verification fails and an error is generated.
+     * If no error is found, then return null.
+     */
+    private ResponseEntity<?> verifyToken(Map<String, String> headers, Map<String, String> body) {
+        // Check for presence of token
+        if (!headers.containsKey("authorization")) {
+            return error("This call requires auth", HttpStatus.UNAUTHORIZED);
+        }
+
+        // Check for presence of user identity
+        if (!body.containsKey("username")) {
+            return error("malformed data", HttpStatus.BAD_REQUEST);
+        }
+
+        // Extract token from header
+        String token = headers.get("authorization").substring(7);
+
+        // Verify JWT token
+        if (!Utils.verifyToken(body.get("username"), token, key)) {
+            return error("bad token", HttpStatus.BAD_REQUEST);
+        }
+
+        // No error to return
+        return null;
+    }
+
+    /**
+     * @param title Key of singleton map
+     * @param data Value of singleton map
+     * @param status HTTP status code to return
+     * @return ResponseEntity representing a singleton response with the specified status code
+     */
+    private ResponseEntity<?> simpleResponse(String title, Object data, HttpStatus status) {
+        return new ResponseEntity<>(Collections.singletonMap(title, data), status);
+    }
+
+    /**
+     * @param error Error message
+     * @param status HTTP status to return
+     * @return ResponseEntity representing error along with status code
+     */
+    private ResponseEntity<?> error(String error, HttpStatus status) {
+        return simpleResponse("error", error, status);
+    }
+
+    /**
+     * @param user The user who is submitting the report
+     * @param report The incoming symptoms report from the user
+     */
+    private void submitReport(User user, Report report) {
+        // Save report to database
         symptomsRepository.save(report);
 
         final int pointsReward = 100;
@@ -175,29 +255,17 @@ public class MainController {
         // Reward user
         user.givePoints(pointsReward);
 
+        // If report was made less than 48 hours from previous report, then the user has submitted a report on
+        // two consecutive days and therefore their streak is incremented. Otherwise, reset streak.
         if (System.currentTimeMillis() - user.getLastReport() <= 2 * 24 * 60 * 60 * 1000) {
             user.incrementStreak();
         } else {
             user.resetStreak();
         }
 
+        // Update user's last report submission time
         user.registerReport();
         userRepository.save(user);
-
-        return new ResponseEntity<>(Collections.singletonMap("success", "report counted"), HttpStatus.OK);
-    }
-
-    @GetMapping("/records/{zipcode}/{start}/{end}")
-    public ResponseEntity<?> getReports(@PathVariable String zipcode, @PathVariable String start, @PathVariable String end) {
-        // Validate arguments
-        if (!Utils.isInteger(zipcode) || !Utils.isInteger(start) || !Utils.isInteger(end)) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "malformed arg"), HttpStatus.BAD_REQUEST);
-        }
-
-        List<Report> reports =
-                symptomsRepository.findAllByTimeBeforeAndTimeAfterAndZipcode(Long.parseLong(end), Long.parseLong(start), Integer.parseInt(zipcode));
-
-        return new ResponseEntity<>(Collections.singletonMap("reports", reports), HttpStatus.OK);
     }
 
 }
